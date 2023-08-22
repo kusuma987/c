d1d9c958961f010b478e088279c06299d5b48e9f
kusuma987 <kambhampatikusuma@gmail.com>
2023-08-22 16:44:34 +0530

    apples added

--- c/c.cpp
+++ c/c.cpp
@@ -5,7 +5,10 @@
 
 int main()
 {
-    std::cout << "Hello World\n";
+   
+	std::cout << "Hello World\n";
+	std::cout << "apples added\n";
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
